f35a73f77a985930b0f7a886232ed0acc8ec6c55
SAKSHAM AGGARWAL <56749406+Saksham-Aggarwal-1@users.noreply.github.com>
2020-06-12 00:01:57 +0530

    Updated models

--- expense_manager/exp_app/models.py
+++ expense_manager/exp_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    User_Id=models.IntegerField(primary_key=True)
+    Username=models.CharField(max_length=30)
+    Password=models.CharField(max_length=30)
+    Currency=models.CharField(max_length=30)
+class Category(models.Model):
+    Category_Id=models.IntegerField(primary_key=True)
+    Name=models.CharField(max_length=30)
+    Budget=models.FloatField()
+class Transaction(models.Model):
+    Transaction_Id=models.IntegerField(primary_key=True)
+    Amount=models.FloatField()
+    Date=models.DateField()
+    Type=models.CharField(max_length=30)
+    Frequency=models.IntegerField()
+    Category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    User=models.ForeignKey(User,on_delete=models.CASCADE)
